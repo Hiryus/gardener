7a6174ace358e863751cbb7fddd7b36c55c6eb8a
Hiryus <hiryus.shadow@gmail.com>
2021-04-06 09:57:49 +0200

    fix: git commit author

--- sources/commit.js
+++ sources/commit.js
@@ -10,7 +10,9 @@ module.exports = function commit(params) {
     console.log('Dependencies update detected, committing packages changes...');
 
     cp.execSync('git add package.json package-lock.json');
-    cp.execSync(`git commit --message "${params.message}" --author "${params.username} <${params.email}>"`);
+    cp.execSync(`git config user.name "${params.username}"`);
+    cp.execSync(`git config user.email "${params.email}"`);
+    cp.execSync(`git commit --message "${params.message}"`);
 
     const hash = cp.execSync('git log --format="%H" -n 1', { encoding: 'utf8' });
 
