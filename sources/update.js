f8dbf87ee4c158039f58acbb33a1c24b1eda62c9
Hiryus <hiryus.shadow@gmail.com>
2021-06-07 10:59:27 +0200

    fix: npm install failing the update job in some cases by returning an error status when everything is fine

--- sources/update.js
+++ sources/update.js
@@ -44,6 +44,11 @@ module.exports = function update(params) {
     // Save changes in packages.json
     fs.writeFileSync('package.json', JSON.stringify(packages, null, 2), 'utf8');
 
-    // Install packages using `npm install` CLI
-    cp.execSync('npm install', { encoding: 'utf8' });
+    // Install packages using `npm install` CLI;
+    // NB: returned status is purposely ignored since the command can return an error status, even
+    // in case of success; instead, the output is parsed to find any error
+    const { output } = cp.spawnSync('npm install', { encoding: 'utf8', shell: true });
+    if (output && output.includes('npm ERR')) {
+        throw new Error(`The command "npm install" failed !\n${output}`);
+    }
 };
